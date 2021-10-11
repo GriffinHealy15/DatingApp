bf348664afb942de27e8e9c1f1d31073dcb822e6
Griffin <griffinparkerhealy@gmail.com>
2021-10-11 14:28:10 -0400

    Fixing wwwroot

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -38,7 +38,6 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member.gender);
     this.memberService.updateMember(this.member).subscribe(() => {
       this.toastr.success('Profile Updated Successfully');
       this.editForm.reset(this.member);
