cc96c53954ca28b6ab39593d253db84908224aa1
Griffin <griffinparkerhealy@gmail.com>
2021-10-12 12:20:09 -0400

    Changed GitIgnore

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,7 +9,7 @@ namespace API.Controllers
         public ActionResult Index()
         {
             return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
-                "heroku_output/wwwroot", "index.html"), "text/HTML");
+                "wwwroot", "index.html"), "text/HTML");
         }
     }
 }
