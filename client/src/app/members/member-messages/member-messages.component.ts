9241ab7f51f8479ad8fd834d88983463fa36d941
Griffin <griffinparkerhealy@gmail.com>
2021-10-13 14:29:56 -0400

    Added Messaging Features

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { take } from 'rxjs/operators';
 import { Message } from 'src/app/_models/message';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
@@ -15,10 +18,14 @@ export class MemberMessagesComponent implements OnInit {
   @Input() username: string;
   messageContent: string;
   loading = false;
+  currentUser: User;
 
-  constructor(public messageService: MessageService) { }
+  constructor(public messageService: MessageService, public accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.currentUser = user;
+    })
   }
 
   // Send Messages
